98168c96f63bf164574654a60b003604d04e6948
VogtD01 <vd6806@mci4me.at>
2024-06-09 23:37:11 +0200

    change side 2 optics

--- seite2.py
+++ seite2.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import person
 from PIL import Image
 import numpy as np
+import myfunctions as mf
 
 
 def seite2():
@@ -14,52 +15,62 @@ def seite2():
     # Lade alle Personen
     person_names = read_person_data.get_person_list(read_person_data.load_person_data())
 
-    # Anlegen diverser Session States
-    ## Gewählte Versuchsperson
-    if 'aktuelle_versuchsperson' not in st.session_state:
-        st.session_state.aktuelle_versuchsperson = 'None'
-
-    ## Anlegen des Session State. Bild, wenn es kein Bild gibt
-    if 'picture_path' not in st.session_state:
-        st.session_state.picture_path = 'data/pictures/none.jpg'
-
-    ## Anlegen des Session State. EKG-Test, wenn es kein EKG-Test gibt
-    if 'ekg_test' not in st.session_state:
-        st.session_state.ekg_test = None
+    # Initialisiere Session State, Versuchperson, Bild, EKG-Test
+    mf.initialize_session_state()
 
     # Schreibe die Überschrift
     st.write("# EKG APP")
-    st.write("## Versuchsperson auswählen")
 
-    # Auswahlbox, wenn Personen anzulegen sind
-    st.session_state.aktuelle_versuchsperson = st.selectbox(
-        'Versuchsperson',
-        options = person_names, key="sbVersuchsperson")
+    # Verwende columns, um die Elemente nebeneinander anzuzeigen
+    col1, col2, col3 = st.columns([1, 1, 2])
 
-    # Name der Versuchsperson
-    st.write("Der Name ist: ", st.session_state.aktuelle_versuchsperson) 
+    with col2:
+        st.write("Versuchsperson auswählen")
+        st.session_state.aktuelle_versuchsperson = st.selectbox(
+            'Versuchsperson',
+            options=person_names, key="sbVersuchsperson")
 
-    # Weitere Daten wie Geburtsdatum etc. schön anzeigen
-    person_dict = read_person_data.find_person_data_by_name(st.session_state.aktuelle_versuchsperson)
-    Person_class = person.Person(person_dict)
+    with col1:
+        if st.session_state.aktuelle_versuchsperson:
+            # Finde den Pfad zur Bilddatei
+            person_dict = read_person_data.find_person_data_by_name(st.session_state.aktuelle_versuchsperson)
+            st.session_state.picture_path = person_dict["picture_path"]
 
-    st.write("## Daten von ", st.session_state.aktuelle_versuchsperson)
-    st.write("Geburtsdatum: ", person_dict["date_of_birth"])
-    st.write("Alter: ", Person_class.calc_age())
-    st.write("ID: ", person_dict["id"])
-    st.write("maximale Herzfrequenz: ", Person_class.calc_max_heart_rate())
+            if st.session_state.picture_path:
+                image = Image.open(st.session_state.picture_path)
+                st.image(image, caption=st.session_state.aktuelle_versuchsperson)
+            else:
+                st.write("Kein Bild verfügbar")
+
+    with col3:
+        if st.session_state.aktuelle_versuchsperson:
+            # Weitere Daten wie Geburtsdatum etc. schön anzeigen
+            person_dict = read_person_data.find_person_data_by_name(st.session_state.aktuelle_versuchsperson)
+            Person_class = person.Person(person_dict)
+
+        # Erste Möglichkeit: Daten in einer Tabelle anzeigen
+        """data = {
+                "Attribut": ["Geburtsdatum", "Alter", "ID", "Maximale Herzfrequenz"],
+                "Wert": [
+                    person_dict["date_of_birth"], 
+                    Person_class.calc_age(), 
+                    person_dict["id"], 
+                    Person_class.calc_max_heart_rate()
+                ]
+            }
+        st.table(data)"""
+
+        # Zweite Möglichkeit: Daten einzeln anzeigen, muss noch entschieden werden
+        st.write("Daten von ", st.session_state.aktuelle_versuchsperson)
+            
+        st.markdown(f"**Geburtsdatum:** {person_dict['date_of_birth']}")
+        st.markdown(f"**Alter:** {Person_class.calc_age()}")
+        st.markdown(f"**ID:** {person_dict['id']}")
+        st.markdown(f"**Maximale Herzfrequenz:** {Person_class.calc_max_heart_rate()}")
 
 
+            
 
-    # Nachdem eine Versuchsperson ausgewählt wurde, die auch in der Datenbank ist
-    # Finde den Pfad zur Bilddatei
-    if st.session_state.aktuelle_versuchsperson in person_names:
-        st.session_state.picture_path = read_person_data.find_person_data_by_name(st.session_state.aktuelle_versuchsperson)["picture_path"]
-        # st.write("Der Pfad ist: ", st.session_state.picture_path) 
-
-    #Bild anzeigen
-    image = Image.open(st.session_state.picture_path)
-    st.image(image, caption=st.session_state.aktuelle_versuchsperson)
 
     #Öffne EKG-Daten
     ekgdata_dict = ekgdata.EKGdata.load_by_id(person_dict["id"])
