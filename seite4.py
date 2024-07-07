3b36f889931cb49539635106ee0dc38493066981
VogtD01 <vd6806@mci4me.at>
2024-07-07 10:04:43 +0200

    add seite4

---
+++ seite4.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import read_person_data
+import ekgdata
+import matplotlib.pyplot as plt
+import person
+from PIL import Image
+import numpy as np
+import streamlit_functions as sf
+
+def seite4(): # Polar Sportanalyse
+    
